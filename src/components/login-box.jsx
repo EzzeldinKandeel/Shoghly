3f9d5a9594c66b8c82b0e9a7ec4c00d456b02402
ezz <ezzeldinkandeel@gmail.com>
2022-04-19 17:21:09 +0200

    set up state in login

--- src/components/login-box.jsx
+++ src/components/login-box.jsx
@@ -2,16 +2,47 @@ import React from "react";
 import "../styles/login.css"
 
 function LoginBox() {
+    const [loginData, setLoginData] = React.useState(
+        {email: "", password: ""}
+    )
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setLoginData(prevLoginData => {
+            return {
+                ...prevLoginData, [name]: value
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     return (
         <div className="form">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="input-container">
                     <label>البريد الإلكتروني: </label>
-                    <input type="email" name="email" className="input-box" required />
+                    <input 
+                        type="email" 
+                        name="email" 
+                        onChange={handleChange} 
+                        value={loginData.email} 
+                        className="input-box" 
+                        required 
+                    />
                 </div>
                 <div className="input-container">
                     <label>الرقم السري: </label>
-                    <input type="password" name="password" className="input-box" required />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        onChange={handleChange}
+                        value={loginData.password}
+                        className="input-box" 
+                        required 
+                    />
                 </div>
                 <div className="button-container">
                     <input type="submit" value="دخول" className="main-button"/>
