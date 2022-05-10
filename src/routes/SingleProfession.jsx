12b6e7fd61b8159f90c11d53fb2aed742e2299e0
Ibrahim Oraby <orabyibrahim19@gmail.com>
2022-05-10 02:49:29 +0200

    fixing nav, favorites, professions in mobile view

--- src/routes/SingleProfession.jsx
+++ src/routes/SingleProfession.jsx
@@ -1,40 +1,38 @@
-import React from "react"
-import { useParams } from "react-router-dom"
-import Footer from "../components/footer"
-import LargeWorkerCard from "../components/LargeWorkerCard"
-import Navbar from "../components/navbar"
-import "../styles/SingleProfession.css"
-import api from "../api/axios"
+import React from "react";
+import { useParams } from "react-router-dom";
+import Footer from "../components/footer";
+import LargeWorkerCard from "../components/LargeWorkerCard";
+import Navbar from "../components/navbar";
+import "../styles/SingleProfession.css";
+import api from "../api/axios";
 
 function SingleProfession() {
-	let params = useParams()
-	let profession = params.profession
+  let params = useParams();
+  let profession = params.profession;
 
-	const [workers, setWorkers] = React.useState([])
-	React.useEffect(async () => {
-		try {
-			const response = await api.get("/users", {
-				params: { profession: profession }
-			})
-			setWorkers(response.data)
-		} catch (err) {
-			console.error(err.message)
-		}
-	},[])
+  const [workers, setWorkers] = React.useState([]);
+  React.useEffect(async () => {
+    try {
+      const response = await api.get("/users", {
+        params: { profession: profession },
+      });
+      setWorkers(response.data);
+    } catch (err) {
+      console.error(err.message);
+    }
+  }, []);
 
-	return (
-		<div className="container">
-			<Navbar />
-			<div className="workers">
-				{workers.map((worker) => {
-					return (
-							<LargeWorkerCard key={worker.id} worker={worker} />
-					)
-				})}
-			</div>
-			<Footer />
-		</div>
-	)
+  return (
+    <div className="container">
+      <Navbar />
+      <div className="workers">
+        {workers.map((worker) => {
+          return <LargeWorkerCard key={worker.id} worker={worker} />;
+        })}
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default SingleProfession
+export default SingleProfession;
