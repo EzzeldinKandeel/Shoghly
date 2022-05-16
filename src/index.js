527472034fc8949f304ada097cc3e1167a9c3423
Ibrahim Oraby <orabyibrahim19@gmail.com>
2022-05-16 20:08:08 +0200

    making a reviews page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import reportWebVitals from "./reportWebVitals"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./routes/login"
-import HomeScreen from "./routes/HomeScreen"
-import Profession from "./routes/professions"
-import SingleProfession from "./routes/SingleProfession"
-import WorkerPage from "./routes/WorkerPage"
-import Favorites from "./routes/Favorites"
-import Conversations from "./routes/Conversations"
-import { UserProvider } from "./context/UserProvider"
-import SignUp from "./routes/SignUp"
-import SettingsPage from "./routes/SettingsPage"
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./routes/login";
+import HomeScreen from "./routes/HomeScreen";
+import Profession from "./routes/professions";
+import SingleProfession from "./routes/SingleProfession";
+import WorkerPage from "./routes/WorkerPage";
+import Favorites from "./routes/Favorites";
+import Conversations from "./routes/Conversations";
+import { UserProvider } from "./context/UserProvider";
+import SignUp from "./routes/SignUp";
+import SettingsPage from "./routes/SettingsPage";
+import ReviewsPage from "./routes/ReviewsPage";
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -32,15 +33,16 @@ ReactDOM.render(
 						<Route path="conversations" element={<Conversations />} />
 						<Route path="worker:workerId" element={<WorkerPage />} />
 						<Route path="settings" element={<SettingsPage />} />
+						<Route path="reviews" element={<ReviewsPage />} />
 					</Route>
 				</Routes>
 			</BrowserRouter>
 		</UserProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
