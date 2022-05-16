49c029776264a5ef9c2f8ff61764812ca15181b4
ezz <ezzeldinkandeel@gmail.com>
2022-05-16 20:35:48 +0200

    added styling for reviews page

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -8,24 +8,26 @@ function Favorites() {
 	const { user } = React.useContext(UserContext)
 	const [favorites, setFavorites] = React.useState(null)
 	React.useEffect(async () => {
-		try {
-			const response = await api.get("/users", {
-				params: { id: user.favoritesId }
-			})
-			setFavorites(response.data)
-		} catch (err) {
-			console.error(err.message)
+		if (user.favoritesId.length) {
+			try {
+				const response = await api.get("/users", {
+					params: { id: user.favoritesId }
+				})
+				setFavorites(response.data)
+			} catch (err) {
+				console.error(err.message)
+			}
 		}
 	}, [])
 
 	return (
-		favorites && (
+		favorites ? (
 			<div className="workers">
 				{favorites.map((worker) => (
 					<LargeWorkerCard key={worker.id} worker={worker} />
 				))}
 			</div>
-		)
+		) : <h1 style={{margin: "auto", fontWeight: "200", color: "gray"}}>لا توجد مفضلات</h1>
 	)
 }
 
