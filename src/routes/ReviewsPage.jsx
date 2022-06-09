1bf183691796150ec305e3536304a65892598e65
ezz <ezzeldinkandeel@gmail.com>
2022-06-09 16:14:29 +0200

    fixed reviews page

--- src/routes/ReviewsPage.jsx
+++ src/routes/ReviewsPage.jsx
@@ -1,21 +1,36 @@
-import React from "react"
-import UserContext from "../context/UserProvider"
+import React, { useContext, useEffect, useState } from "react"
 import ReviewBox from "../components/ReviewBox"
+import AuthContext from "../context/AuthProvider"
+import api from "../api/axios"
 import "../styles/ReviewsPage.css"
 
 function ReviewsPage() {
-	const { user } = React.useContext(UserContext)
-	console.log(user.reviews)
-	
-	return (
+	const { auth } = useContext(AuthContext)
+	const [reviews, setReviews] = useState([])
+	useEffect(async () => {
+		try {
+			const response = await api.get(`/workers/${auth.id}/reviews`, {
+				headers: { Authorization: `Bearer ${auth.token}` }
+			})
+			setReviews(response.data.reviews)
+		} catch (err) {
+			console.error(err)
+		}
+	}, [])
+
+	return reviews.length ? (
 		<div className="reviews-container">
 			<h2>جميع تقييماتك</h2>
 			<div className="review-boxes-container">
-				{user.reviews.map((review) => (
-					<ReviewBox key={user.reviews.indexOf(review)} review={review} />
+				{reviews.map((review) => (
+					<ReviewBox key={review.reviewId} review={review} />
 				))}
 			</div>
 		</div>
+	) : (
+		<h1 style={{ margin: "auto", fontWeight: "200", color: "gray" }}>
+			لا توجد تعليقات
+		</h1>
 	)
 }
 
